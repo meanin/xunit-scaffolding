93494498b94ad977e92217e14511e259b66c60ed
Paweł Ruciński <pawel.rucinski@cybercom.com>
2017-11-15 17:32:30 +0100

    Create scaffolding for xunit test project and classes with installer

---
+++ Xunit Test Project/Tests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Xunit_Test_Project
+{
+    public class Tests
+    {
+        [Fact]
+        public void Method_When_Should()
+        {
+            // arrange
+
+            // act
+
+            // assert
+
+        }
+    }
+}
